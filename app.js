33f2fd1f2ee76f78cbd4eb76b473489337187472
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-04 22:04:17 +0100

    Created an http server

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
+
+const http = require('http');
+const server = http.createServer(app);
+
 const port = 3000
 
 app.use(express.static('public'))
