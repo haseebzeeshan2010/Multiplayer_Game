aa92969c0ebf2a32ccb3f85e6aa3f4ef822361bd
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-04 22:15:00 +0100

    Getting the server object associated with library

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 
+//socket.io setup
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 const port = 3000
 
