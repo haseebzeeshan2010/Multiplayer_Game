ca10eaf27bea79faac9b7b26c24efdd9d097e45d
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-06-17 20:49:20 +0100

    Made code cleaner to read

--- backend.js
+++ backend.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
 
-//socket.io setup
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server, {pingInterval: 2000, pingTimeout: 5000});
+// socket.io setup
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 5000 })
 
 const port = 3000
 
@@ -15,51 +15,57 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-const backendEndPlayers = {}
-const SPEED = 10//original was 10
+const backEndPlayers = {}
+
+const SPEED = 10
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  backendEndPlayers[socket.id] = {
-    x: 500*Math.random(),
-    y: 500*Math.random(),
-    color: `hsl(${360*Math.random()}, 100%, 50%)`,
-    sequenceNumber:0
+  console.log('a user connected')
+  backEndPlayers[socket.id] = {
+    x: 500 * Math.random(),
+    y: 500 * Math.random(),
+    color: `hsl(${360 * Math.random()}, 100%, 50%)`,
+    sequenceNumber: 0
   }
 
-  io.emit('updatePlayers', backendEndPlayers)
+  io.emit('updatePlayers', backEndPlayers)
 
   socket.on('disconnect', (reason) => {
     console.log(reason)
-    delete backendEndPlayers[socket.id]
-    io.emit('updatePlayers',backendEndPlayers)
+    delete backEndPlayers[socket.id]
+    io.emit('updatePlayers', backEndPlayers)
   })
-  socket.on('keydown', ({keycode, sequenceNumber}) => {
-    backendEndPlayers[socket.id].sequenceNumber = sequenceNumber
-    switch(keycode){
+
+  socket.on('keydown', ({ keycode, sequenceNumber }) => {
+    backEndPlayers[socket.id].sequenceNumber = sequenceNumber
+    switch (keycode) {
       case 'KeyW':
-        backendEndPlayers[socket.id].y -= SPEED
+        backEndPlayers[socket.id].y -= SPEED
         break
+
       case 'KeyA':
-        backendEndPlayers[socket.id].x -= SPEED
+        backEndPlayers[socket.id].x -= SPEED
         break
+
       case 'KeyS':
-        backendEndPlayers[socket.id].y += SPEED
+        backEndPlayers[socket.id].y += SPEED
         break
+
       case 'KeyD':
-        backendEndPlayers[socket.id].x += SPEED
+        backEndPlayers[socket.id].x += SPEED
         break
     }
   })
-});
 
+  console.log(backEndPlayers)
+})
 
-setInterval(() =>{
-  io.emit('updatePlayers',backendEndPlayers)
-},15)//original was 15
-
-
+setInterval(() => {
+  io.emit('updatePlayers', backEndPlayers)
+}, 15)
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+console.log('server did load')
